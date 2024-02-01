66a4986b9ce74343e1449af9bb34bd18e06518df
Justin Rinehuls <jrinehuls@gmail.com>
2024-01-31 22:42:51 -0500

    Format stats bos

--- src/components/monster_cards/MonsterCard.jsx
+++ src/components/monster_cards/MonsterCard.jsx
@@ -15,14 +15,22 @@ function MonsterCard( {monster} ) {
                 <img className={styles.image} src={`data:image/${monster.imageExtension};base64,${monster.image}`}></img>
             </div>
             <div className={styles.statsContainer}>
-                <div>Attack:{monster.attack}</div>
-                <div>Defense:{monster.defense}</div>
-                <div>Speed:{monster.speed}</div>
-                <div>MP:{monster.mp}</div>
-                <div>Magic Attack:{monster.magicAttack}</div>
-                <div>Magic Defense:{monster.magicDefense}</div>
-                <div>Exp:{monster.baseExp}</div>
-                <div>Gold:{monster.baseGold}</div>
+                <div className={styles.statsRow}>
+                    <div>Attack:{monster.attack}</div>
+                    <div>Defense:{monster.defense}</div>
+                </div>
+                <div className={styles.statsRow}>
+                    <div>Speed:{monster.speed}</div>
+                    <div>MP:{monster.mp}</div>
+                </div>
+                <div className={styles.statsRow}>
+                    <div>Magic Attack:{monster.magicAttack}</div>
+                    <div>Magic Defense:{monster.magicDefense}</div>
+                </div>
+                <div className={styles.statsRow}>
+                    <div>Exp:{monster.baseExp}</div>
+                    <div>Gold:{monster.baseGold}</div>
+                </div>
             </div>
             <div className={styles.spellContainer}>
                 Spells will go here
